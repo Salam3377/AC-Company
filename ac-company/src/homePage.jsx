73dfd118f3630893b37768afcf25b56dbeb4358d
Salam337 <salamadash@gmail.com>
2023-09-16 22:00:45 -0400

    added some navbar, home screen and logo

--- ac-company/src/homePage.jsx
+++ ac-company/src/homePage.jsx
@@ -1,1 +1,18 @@
-hello 
+import React from 'react'
+import ACLogo from './ACLogo.png'
+import './CSS/homePage.css'
+
+function homePage() {
+  return (
+    <div id='home-main-div'>
+
+      <div id='img-div'>
+        <img id='logo-img' src={ACLogo} />
+      </div>
+
+      {/* down here need inline list of page options */}
+    </div>
+  )
+}
+
+export default homePage
