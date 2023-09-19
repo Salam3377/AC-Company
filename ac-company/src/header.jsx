c44e9b4545132bd2e1998db007885de201c25189
Salam337 <salamadash@gmail.com>
2023-09-18 21:25:06 -0400

    added header as separate component

---
+++ ac-company/src/header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ACLogo from './ACLogo.png'
+import './CSS/header.css'
+
+function header() {
+  return (
+    <div id='home-main-div'>
+
+      <div id='logo-div'>
+        <span><img id='logo-img' src={ACLogo} /></span>
+        <span id='header-name'>Kumuk Air 84</span>
+      </div>
+
+      <div id='div-carousel'></div>
+
+      {/* down here need inline list of page options */}
+    </div>
+  )
+}
+
+export default header
--- ac-company/src/homePage.jsx
+++ ac-company/src/homePage.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import ACLogo from './ACLogo.png'
 import './CSS/homePage.css'
+import Header from './header'
 
 function homePage() {
   return (
     <div id='home-main-div'>
+      <Header />
 
-      <div id='logo-div'>
-        <span><img id='logo-img' src={ACLogo} /></span>
-        <span id='header-name'>Kumuk Air 84</span>
-      </div>
+      <div id='div-carousel'></div>
 
-      {/* down here need inline list of page options */}
     </div>
   )
 }
