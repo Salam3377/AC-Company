c1bb85985c59ae65305dd834f0c6867d68181dc6
nguyenpham207 <seanphamgd@gmail.com>
2023-10-03 14:08:47 -0700

    emailjs without css

--- ac-company/src/Components/contact.jsx
+++ ac-company/src/Components/contact.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-function contact() {
+function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_dfboqvi', 'contact_form', form.current, 'i2GbDsMhksqcR1bad')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
+
   return (
-    <div style={{width: "fitContent", textAlign: "center"}}>
-        <h1>Contact Us</h1>
-    </div>
-  )
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type='text' name="user_name"></input>
+      <label>Email</label>
+      <input type="email" name="user_email"></input>
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="send" />
+    </form>
+  );
 }
 
-export default contact
+export default Contact;
