041cec3aae5c41906c9e26275f105deed49b6a06
Salam337 <salamadash@gmail.com>
2023-10-08 13:04:30 -0400

    changed button className to correct css

--- ac-company/src/Components/contact.jsx
+++ ac-company/src/Components/contact.jsx
@@ -20,24 +20,6 @@ function Contact() {
   return (
     <form ref={form} onSubmit={sendEmail}>
 
-
-    <div className='field-input'>
-      <label>Name</label>
-    </div>
-      <input className='box' type='text' name="user_name"></input>
-    <div className='field-input'>
-      <label>Email</label>
-    </div>
-      <input className='box' type="email" name="user_email"></input>
-    <div className='field-input'>
-      <label>Message</label>
-    </div>
-      <textarea className='box' name="message" />
-    <div className='field-input'>
-      <input type="submit" value="send"/>
-    </div>
-
-
     <div className='title'>
       <div>
       <label className='title-label'>Name</label>
@@ -54,7 +36,7 @@ function Contact() {
       <label>Message</label>
       <textarea className='test' placeholder='message...' type= "text" name="message" />
     </div>
-    <button>
+    <button className='buttonEmail'>
       Send
     </button>
 
